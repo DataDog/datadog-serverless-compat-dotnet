61382060ff922abfdabed666b0937a0363dcf09f
Lucas Pimentel <lucaspimentel@gmail.com>
2025-08-15 18:47:44 -0400

    add sample app to test trace agent

---
+++ SendEmptyTrace/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Headers;
+
+while (true)
+{
+    try
+    {
+        // send empty an array in MessagePack format
+        var content = new ByteArrayContent([0x90]);
+        content.Headers.ContentType = new MediaTypeHeaderValue("application/msgpack");
+
+        // add these headers to every request
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Datadog-Meta-Tracer-Version", "3.25.0.0");
+        client.DefaultRequestHeaders.Add("Datadog-Meta-Lang", ".NET");
+        client.DefaultRequestHeaders.Add("Datadog-Meta-Lang-Interpreter", ".NET"); // ".NET", ".NET Core", or ".NET Framework"
+        client.DefaultRequestHeaders.Add("Datadog-Meta-Lang-Version", Environment.Version.ToString());
+        client.DefaultRequestHeaders.Add("X-Datadog-Trace-Count", "0");
+
+        // try to send the empty trace to the trace agent
+        var response = await client.PostAsync("http://localhost:8126/v0.4/traces", content);
+        response.EnsureSuccessStatusCode();
+
+        Console.WriteLine("Successfully sent empty trace to Datadog agent");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Failed to send trace: {ex.Message}");
+    }
+
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
