714c89f7bf5d6958860017ff3461bdc847c7fc50
Lucas Pimentel <lucaspimentel@gmail.com>
2025-08-15 18:48:26 -0400

    add startup hook

---
+++ Datadog.Serverless/StartupHook.cs
@@ -0,0 +1,28 @@
+﻿using Datadog.Serverless;
+using Datadog.Serverless.Logging;
+
+// https://github.com/dotnet/runtime/blob/main/docs/design/features/host-startup-hook.md
+
+// ReSharper disable once UnusedType.Global
+// ReSharper disable once CheckNamespace
+internal static class StartupHook
+{
+    // ReSharper disable once UnusedMember.Global
+    public static void Initialize()
+    {
+        Logger? logger = null;
+
+        try
+        {
+            var logLevel = Logger.GetLogLevelFromEnvironment();
+            logger = new Logger(Console.Out, nameof(StartupHook), logLevel);
+            logger.LogInformation("Starting the Datadog Serverless Compatibility Layer.");
+
+            CompatibilityLayer.Start();
+        }
+        catch (Exception ex)
+        {
+            logger?.LogError(ex, "Error starting the Datadog Serverless Compatibility Layer.");
+        }
+    }
+}
